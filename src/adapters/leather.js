8f5b49cdef99ad10e2357e0748015b3139870e52
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2023-10-05 15:22:53 +0800

    fixed leather wallet crypto.randomUUID problem

--- src/adapters/leather.js
+++ src/adapters/leather.js
@@ -1,33 +1,44 @@
 module.exports = leather = (provider) => {
-    
-    let wallet = window.LeatherProvider;
+
+    if (window.crypto && !window.crypto.randomUUID) {
+        window.crypto.randomUUID = () => {
+            let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                const r = (Math.random() * 16) | 0;
+                const v = c === 'x' ? r : (r & 0x3) | 0x8;
+                return v.toString(16);
+            });
+            return uuid;
+        };
+    }
+
     const network =  provider.testnet ? 'testnet' : 'mainnet';
 
-    wallet.sendBitcoin = (to, amount) => {
-        return new Promise(async (resolve, reject) => {
-            try {
-                await wallet.request('sendTransfer', {
-                    address: to,
-                    amount,
-                    network
-                })
-                .then((response) => {
-                    resolve(response.result.txid);
-                })
-                .catch(({error}) => {
-                    reject(error);
-                });
-            } catch (error) {
-                reject(error)
+    let wallet = Object.assign({
+        sendBitcoin: (to, amount) => {
+            return new Promise(async (resolve, reject) => {
+                try {
+                    await wallet.request('sendTransfer', {
+                        address: to,
+                        amount,
+                        network
+                    })
+                    .then((response) => {
+                        resolve(response.result.txid);
+                    })
+                    .catch(({error}) => {
+                        reject(error);
+                    });
+                } catch (error) {
+                    reject(error)
+                }
+            });
+        },
+        on: (event, callback) => {
+            if (window.btc && btc.listen) {
+                btc.listen(event, callback);
             }
-        });
-    }
-
-    wallet.on = (event, callback) => {
-        if (window.btc && btc.listen) {
-            btc.listen(event, callback);
         }
-    }
+    }, window.LeatherProvider);
 
     const connect = async () => {
         return new Promise(async (resolve, reject) => {
@@ -40,6 +51,8 @@ module.exports = leather = (provider) => {
                 // for ordinals & BRC-20 integrations
                 // const ordinals = addresses.find(address => address.type == 'p2tr');
 
+                wallet = Object.assign(wallet, window.LeatherProvider);
+                
                 wallet.getAddress = async () => {
                     return bitcoin.address;
                 }
